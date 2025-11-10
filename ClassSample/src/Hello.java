d8042e54f5178cd2ea5b4701787ae191f77e558b
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:11:21 +0530

    final commit

---
+++ ClassSample/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+	public static void main(String ar[]) {
+		
+		Sample s1=new Sample();
+		Sample s2=new Sample();
+		
+		 s1.a=10;
+		 s2.a=20;
+		 s1.b=30;
+		 s2.b=40;
+		 
+		s1.display();
+		s2.display();
+		
+		
+		
+	}
+}
