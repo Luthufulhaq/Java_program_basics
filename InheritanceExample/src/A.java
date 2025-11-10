7a4c013a577c10cd92e56d3d81158450ad67c723
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:13:09 +0530

    final commit

---
+++ InheritanceExample/src/A.java
@@ -0,0 +1,9 @@
+
+public class A {
+	int a=10;
+	void displayA() {
+		System.out.println("Welcome to class A");
+		System.out.println(a);
+	}
+
+}
---
+++ InheritanceExample/src/B.java
@@ -0,0 +1,8 @@
+
+public class B extends A{
+	int num=20;
+	void displayB() {
+		System.out.println("Welcome to class B");
+		System.out.println(num);
+	}
+}
