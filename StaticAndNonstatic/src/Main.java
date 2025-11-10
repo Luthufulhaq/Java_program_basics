e2ca15d422269f0ca28db67cec745bb62e8bab24
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:14:26 +0530

    final commit

---
+++ StaticAndNonstatic/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	public static void main(String huhu[]) {
+		
+		
+		int c=1111;
+		
+		
+		Values v=new Values();
+		Values v1=new Values();
+		System.out.println(v.a+" "+v.b);
+
+		
+		v1.a=1000;
+		v1.b=1000;
+		
+		c=2222;
+		
+		System.out.println(v.a+" "+v.b);
+		System.out.println(v1.a+" "+v1.b);
+		System.out.println(c);	
+	}
+}
---
+++ StaticAndNonstatic/src/Values.java
@@ -0,0 +1,6 @@
+
+public class Values {
+	int a=100;
+	static int b=500;
+	
+}
