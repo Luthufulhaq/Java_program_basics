675224fa9d292fec26b534b0547af5b78f88fd34
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:14:51 +0530

    final commit

---
+++ SumWithTwoClasses/src/Dsiplay.java
@@ -0,0 +1,24 @@
+
+public class Dsiplay {
+	public static void main(String h[]) {
+		
+		int num1=100;
+		int num2=200;
+		
+		int num3=150;
+		int num4=250;
+		
+		Sum s=new Sum();
+		Sum s1=new Sum();
+		
+		s.calculateSum(num1, num2);
+		s.displaySum();
+		
+		s1.calculateSum(num3, num4);
+		s1.displaySum();
+		
+		
+		
+		
+	}
+}
---
+++ SumWithTwoClasses/src/Sum.java
@@ -0,0 +1,15 @@
+
+public class Sum {
+	
+		int c;
+		
+		void calculateSum(int k,int m) {
+			
+			c=k+m;
+		}
+		void displaySum() {
+			
+			System.out.println(c);
+		}
+	
+}
