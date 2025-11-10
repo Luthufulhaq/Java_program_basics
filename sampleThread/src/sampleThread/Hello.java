1d12e88374ef728aa5eb1dead5f1f85033f247cd
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:14:01 +0530

    final commit

---
+++ sampleThread/src/sampleThread/Hello.java
@@ -0,0 +1,15 @@
+package sampleThread;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		SampleThread st1=new SampleThread(1);
+		SampleThread st2=new SampleThread(2);
+		SampleThread st3=new SampleThread(3);
+		
+		st1.start();
+		st2.start();
+		st3.start();
+		
+	}
+}
