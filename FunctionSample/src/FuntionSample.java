59fcaa1c4edc436f587619bf3b84eb5bea7137b2
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:12:31 +0530

    final commit

---
+++ FunctionSample/src/FuntionSample.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FuntionSample {
+	public static void main(String ar[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any 2 numbers :");
+		
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int result=sum(num1,num2);
+		
+		System.out.println("Result :"+result);
+	}
+	
+	
+	static int sum(int a,int b) {
+		int s=a+b;
+		return s;
+	}
+}
