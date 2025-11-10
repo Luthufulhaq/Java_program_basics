ab67efce547e2d7d45901e97c503b1908562666d
luthu <luthu@DESKTOP-9452V7Q>
2025-11-10 12:13:23 +0530

    final commit

---
+++ Input_and_output/src/print_2_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class print_2_numbers {
+	
+	public static void main(String arg[]) {
+		
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("Enter 2 numbers :");
+		
+		
+			int a=s.nextInt();
+			int b=s.nextInt();
+			
+			
+		System.out.println("The numbers are "+a+"  "+b);
+			int c;
+			c=a+b;
+		System.out.println("Sum of the number is :"+c);
+		
+	}
+
+}
